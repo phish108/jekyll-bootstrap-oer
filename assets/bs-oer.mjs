2fb8469c866a7d6996a01d9a4b89257b08cb2651
Christian Glahn <cpglahn@gmail.com>
2023-04-24 21:28:12 +0200

    nicer toc toggling

--- assets/bs-oer.mjs
+++ assets/bs-oer.mjs
@@ -1,7 +1,6 @@
 document.querySelector(".toc-toggle").addEventListener("click", (ev) => {
-    document.querySelector(".toc").classList.toggle("show");
-    document.querySelector(".TOC").classList.toggle("show");
+    document.querySelector(".sidebar").classList.toggle("show");
     const tt = document.querySelector(".toc-toggle .bi");
-    tt.classList.toggle(".bi-list");
-    tt.classList.toggle(".bi-x-lg");
+    tt.classList.toggle("bi-list");
+    tt.classList.toggle("bi-x-lg");
 });
