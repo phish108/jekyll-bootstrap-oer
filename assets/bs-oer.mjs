c24d86993c481e17fba362065eb97335bed6a091
Christian Glahn <cpglahn@gmail.com>
2023-04-24 20:06:48 +0200

    remove unnessecary spacing from TOC header

--- assets/bs-oer.mjs
+++ assets/bs-oer.mjs
@@ -1,3 +1,4 @@
 document.querySelector(".toc-toggle").addEventListener("click", (ev) => {
     document.querySelector(".toc").classList.toggle("show");
-    document.querySelector(".TOC").classList.toggle("show");});
+    document.querySelector(".TOC").classList.toggle("show");
+});
