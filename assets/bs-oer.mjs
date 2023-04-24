1602842138e1d962f91b73409bb054f10b8779fe
Christian Glahn <cpglahn@gmail.com>
2023-04-24 19:58:04 +0200

    toggle all TOC elements

--- assets/bs-oer.mjs
+++ assets/bs-oer.mjs
@@ -1,3 +1,3 @@
 document.querySelector(".toc-toggle").addEventListener("click", (ev) => {
     document.querySelector(".toc").classList.toggle("show");
-});
+    document.querySelector(".TOC").classList.toggle("show");});
